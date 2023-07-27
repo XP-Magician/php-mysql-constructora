5d74daa6c5b0bec1abec08c52601fb31e8888c1b
Diego <diegofabrega18@gmail.com>
2023-07-26 20:47:38 -0400

    UPDATE: Modificada la estructura de llamadas a env variables

--- work/Libs/Config.php
+++ work/Libs/Config.php
@@ -3,5 +3,5 @@
     CONST DB_USER = "if0_34695521";
     CONST DB_NAME = "if0_34695521_Cke";
     CONST HOST = "sql113.infinityfree.com";
-    CONST PASSWORD = "c30h9k1j"
+    CONST PASSWORD = "c30h9k1j";
 ?>
--- work/Model/bdConfig.php
+++ work/Model/bdConfig.php
@@ -3,21 +3,14 @@ require_once("../work/Libs/Config.php");
 class Database
 {
     public $conn;
-    private $host;
-    private $db_name;
-    private $username;
-    private $password;
     private $bandera = 1;
 
 function dbConnection()
 {
-    $this->host = $HOST;
-    $this->db_name = $DB_NAME;
-    $this->username = $DB_USER;
-    $this->password = $PASSWORD;
+    
     $this->conn = null;
     try {
-        $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+        $this->conn = new PDO("mysql:host=" . HOST. ";dbname=" . DB_NAME, DB_USER, PASSWORD);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $exception) {
         if ($this->bandera == 1) {
